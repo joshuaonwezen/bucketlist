77d9c2e0656aecc9d7f2b2f2f81cca66567ca9be
Joshua Onwezen <joshua.onwezen@gmail.com>
2021-11-30 14:11:13 +0100

    Initial commit

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,24 @@
-<script>
-	import '../app.css';
+<script context="module">
+	export const load = async ({ fetch }) => {
+		const res = await fetch('/pages.json');
+		if (res.ok) {
+			const { pages } = await res.json();
+			return {
+				props: { pages }
+			};
+		}
+	};
 </script>
 
-<slot />
+<script>
+	import Nav from '$lib/nav.svelte';
+	import '../app.css';
+
+	export let pages
+</script>
+
+<Nav {pages}/>
+
+<main class="container max-w-xl mx-auto px-4">
+	<slot />
+</main>
