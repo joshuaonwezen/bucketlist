36bcd761b7008250f5b00a1eaa041092df8b541e
Joshua Onwezen <joshua.onwezen@gmail.com>
2022-02-22 21:25:14 +0100

    first version incl localstorage

--- src/lib/nav.svelte
+++ src/lib/nav.svelte
@@ -1,26 +1,20 @@
-
 <script>
-    export let pages
+	export let pages;
 </script>
 
 <div class="navbar mb-2 shadow-lg bg-neutral text-neutral-content rounded-box">
-    <div class="px-2 mx-2 navbar-start">
-      <span class="text-lg font-bold">
-              NOVA
-            </span>
-    </div> 
-    <div class="hidden px-2 mx-2 navbar-center lg:flex">
-      <div class="flex items-stretch">
-            <a href={`/`} class="btn btn-ghost btn-sm rounded-btn">
-                Home
-            </a> 
-            {#each pages as {title, slug}}
-                <a href={`/${slug}`} class="btn btn-ghost btn-sm rounded-btn">
-                    {title}
-                </a> 
-            {/each}
-      </div>
-    </div> 
-    <div class="navbar-end">
-    </div>
-  </div>
+	<div class="px-2 mx-2 navbar-start">
+		<span class="text-lg font-bold"> 🐯 </span>
+	</div>
+	<div class="hidden px-2 mx-2 navbar-center lg:flex">
+		<div class="flex items-stretch">
+			<a href={`/`} class="btn btn-ghost btn-sm rounded-btn"> Home </a>
+			{#each pages as { title, slug }}
+				<a href={`/${slug}`} class="btn btn-ghost btn-sm rounded-btn">
+					{title}
+				</a>
+			{/each}
+		</div>
+	</div>
+	<div class="navbar-end" />
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -11,13 +11,30 @@
 </script>
 
 <script>
-	import { score } from '../stores'
+	import { checkedAnswers } from '../stores'
+	import { onMount } from 'svelte'
 	export let categories;
+	let answers = [],
+		results = 0;
 
-	function calculateScore(){
+	onMount(async() => {
+		checkedAnswers.useLocalStorage();
+	});
 
-		score.update(score)
-		console.log(score)
+	const calculateScore = () => {
+		results = 0;
+		for(const answer in answers){
+			results += parseInt(document.querySelector(`input[value="${answers[answer]}"]`).getAttribute('data-value'));
+		}
+		return results;
+	}
+
+	const transformToId = (value) => {
+		return value.toLowerCase().replace(/\//g, '').replace(/ +/g, "-");
+	}
+
+	const addToArray = (e, value) => {
+		$checkedAnswers = e.checked ? [...$checkedAnswers, value] : $checkedAnswers.filter(item => item !== value)
 	}
 </script>
 
@@ -25,43 +42,33 @@
 	<title>Tigerpoints</title>
 </svelte:head>
 
-<h1 class="text-4xl mb-10 font-extrabold text-center">Tigerpoints</h1>
-<form>
-	{#each categories as { name, question }}
-		<div class="card text-center shadow-2xl mb-20">
-			<h1 class="card-title">
-				{name}
-			</h1>
-			<div class="card-body flex flex-row flex-wrap mb-4">
-				{#each question as { name, value }}
-					<div class="form-control w-1/3">
-						<label class="cursor-pointer flex p-1">
-							<input type="checkbox" data-value={value} class="checkbox checkbox-primary" />
-							<span class="label-text ml-1">{name}</span>
-						</label>
-					</div>
-				{/each}
-			</div>
+<h1 class="text-4xl mb-10 font-extrabold text-center">🐯 Tigerpoints 🐯</h1>
+{#each categories as { name, question }}
+	<div class="card text-center shadow-2xl mb-20">
+		<h1 class="card-title">
+			{name}
+		</h1>
+		<div class="card-body flex flex-row flex-wrap mb-4">
+			{#each question as { name, value }}
+				<div class="form-control w-1/3">
+					<label class="cursor-pointer flex p-1 mb-3">
+						<input type="checkbox" checked={$checkedAnswers.includes(transformToId(name)) ? 'checked' : ''} value={transformToId(name)} bind:group={answers} on:change={addToArray(this, transformToId(name))} data-value={value} class="checkbox checkbox-primary min-w-[24px]" />
+						<span class="label-text ml-2 text-left">{name}</span>
+					</label>
+				</div>
+			{/each}
 		</div>
-		<!-- <div class="card text-center shadow-2xl mb-20">
-		<figure class="px-10 pt-10">
-			<img src={coverImage.url} alt={`Cover image for ${title}`} />
-		</figure>
-        <div class="card-body">
-            <h2 class="title">{title}</h2>
-            <p>{excerpt}</p>
-            <div class="flex justify-center mt-5 space-x-2">
-                {#each tags as tag}
-                    <span class="badge badge-primary">{tag}</span>    
-                {/each}
-            </div>
-            <div class="justify-center card-actions">
-                <a href={`/posts/${slug}`} class="btn btn-outline btn-primary">
-                    Read &rArr;
-                </a>
-            </div>
-        </div>
-	</div> -->
-	{/each}
-	<input type="submit" class="btn" value="Calculate score" on:submit={calculateScore}>
-</form>
+	</div>
+{/each}
+<label for="result-modal" class="btn modal-button" on:click={calculateScore}>Calculate Score</label>
+
+<input type="checkbox" id="result-modal" class="modal-toggle">
+<div class="modal">
+  <div class="modal-box">
+    <h3 class="font-bold text-lg"> 🐯 Keep it up. 🐯</h3>
+    <p class="py-4">You have {results} tigerpoints.</p>
+    <div class="modal-action">
+      <label for="result-modal" class="btn">Click me for hot singles near you!</label>
+    </div>
+  </div>
+</div>
